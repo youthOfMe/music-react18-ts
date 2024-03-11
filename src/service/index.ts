f8ee1138e7f3bb3900ac4b5ca15795adc4c58659
youthOfMe <2040690180@qq.com>
2024-03-11 22:03:27 +0800

    feat: 引入aixos和配置axios请求封装工具

---
+++ src/service/index.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://coderwhy.dev:8000'
+export const TIME_OUT = 10000
---
+++ src/service/request/index.ts
@@ -0,0 +1,76 @@
+import type { AxiosInstance } from "axios";
+import type { XHRequestConfig } from "./type";
+import axios from "axios";
+
+class XHRequest {
+    instance: AxiosInstance
+
+    // request实例 => axios实例
+    constructor(config: XHRequestConfig) {
+        this.instance = axios.create(config)
+
+        // 每个instance实例都添加拦截器
+        this.instance.interceptors.request.use((config) => {
+            console.log('全局请求成功的拦截器')
+            return config
+        }, (err) => {
+            console.log('全局请求失败的拦截器')
+            return err
+        })
+        this.instance.interceptors.response.use((res) => {
+            console.log('全局响应成功的拦截器')
+            return res.data
+        }, (err) => {
+            console.log('全局响应失败的拦截器')
+            return err
+        })
+
+        // 针对特定的xhRequest实例添加拦截器
+        this.instance.interceptors.request.use(
+            config.interceptors?.requestSuccessFunc,
+            config.interceptors?.requestFailureFunc
+        )
+        this.instance.interceptors.response.use(
+            config.interceptors?.responseSuccessFunc,
+            config.interceptors?.responseFailureFunc
+        )
+    }
+
+    // 封装网络请求的方法
+    request<T = any>(config: XHRequestConfig<T>) {
+        // 单次请求的成功拦截处理
+        config = config.interceptors?.requestSuccessFunc ? config.interceptors.requestSuccessFunc(config) : config
+
+        return new Promise<T>((resolve, reject) => {
+            this.instance.request<any, T>(config).then(res => {
+                res = config.interceptors?.responseSuccessFunc ? config.interceptors.responseSuccessFunc(res) : res
+                resolve(res)
+            }).catch(err => {
+                reject(err)
+            })
+        })
+    }
+
+    // 进行封装其他网络请求
+    get<T = any>(config: XHRequestConfig<T>) {
+        return this.request({ ...config, method: 'GET' })
+    }
+
+    post<T = any>(config: XHRequestConfig<T>) {
+        return this.request({ ...config, method: 'POST' })
+    }
+
+    delete<T = any>(config: XHRequestConfig<T>) {
+        return this.request({ ...config, method: 'DELETE' })
+    }
+
+    put<T = any>(config: XHRequestConfig<T>) {
+        return this.request({ ...config, method: 'PUT' })
+    }
+
+    patch<T = any>(config: XHRequestConfig<T>) {
+        return this.request({ ...config, method: 'PATCH' })
+    }
+}
+
+export default XHRequest
