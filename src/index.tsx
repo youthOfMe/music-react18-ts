298f9a616681c8c7c3f09d965820c3e239377818
youthOfMe <2040690180@qq.com>
2024-03-17 23:17:36 +0800

    feat: 进行修复全局样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './assets/css/index.less'
+import 'normalize.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
