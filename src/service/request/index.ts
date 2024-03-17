ae5bf265e1f30d788913e723b27c6716a1cd7f18
youthOfMe <2040690180@qq.com>
2024-03-17 22:40:37 +0800

    feat: 配置请求工具包

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -12,21 +12,17 @@ class XHRequest {
     // 每个instance实例都添加拦截器
     this.instance.interceptors.request.use(
       (config) => {
-        console.log('全局请求成功的拦截器')
         return config
       },
       (err) => {
-        console.log('全局请求失败的拦截器')
         return err
       },
     )
     this.instance.interceptors.response.use(
       (res) => {
-        console.log('全局响应成功的拦截器')
         return res.data
       },
       (err) => {
-        console.log('全局响应失败的拦截器')
         return err
       },
     )
