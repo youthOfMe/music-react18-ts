90a00e16d5029274c903616600ce762531cd5b39
youthOfMe <2040690180@qq.com>
2024-03-17 22:57:47 +0800

    feat: 修复代码

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,9 +1,2 @@
-// const {
-//   REACT_APP_BASE_URL,
-//   REACT_APP_TIME_OUT
-// } = process.env
-
-console.log(process.env.REACT_APP_BASE_URL)
-
 export const BASE_URL = process.env.REACT_APP_BASE_URL
 export const TIME_OUT = process.env.REACT_APP_TIME_OUT
