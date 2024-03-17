028bd25038de5a04e4a27f14fb03d6e722aa8c7f
youthOfMe <2040690180@qq.com>
2024-03-17 23:41:08 +0800

    feat: 搭建App顶部左部样式

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -2,6 +2,8 @@ import React, { memo } from 'react'
 import type { ReactNode } from 'react'
 import { HeaderLeft, HeaderRight, HeaderWrapper } from './style'
 
+import HeaderTitles from '@/assets/data/header-titles.json'
+
 interface IProps {
   children?: ReactNode
 }
@@ -14,6 +16,15 @@ const AppHerder: React.FC<IProps> = (props) => {
           <a className="logo sprite_01" href="/">
             网易云音乐
           </a>
+          <div className="title-list">
+            {HeaderTitles.map((item) => {
+              return (
+                <div className="item active" key={item.link}>
+                  <a href="">{item.title}</a>
+                </div>
+              )
+            })}
+          </div>
         </HeaderLeft>
         <HeaderRight></HeaderRight>
       </div>
