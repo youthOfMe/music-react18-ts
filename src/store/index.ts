dd7ede4ecdcfda1e54d5a2ee711cdfc9cbb9f9f6
youthOfMe <2040690180@qq.com>
2024-03-20 23:56:11 +0800

    feat: 搭建首页-推荐页数据维持仓库

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './modules/counter'
 import { useSelector, TypedUseSelectorHook, useDispatch, shallowEqual } from 'react-redux'
+import recommendReducer from '@/views/discover/c-views/recommend/store/recommend'
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    recommmend: recommendReducer,
   },
 })
 
---
+++ src/views/discover/c-views/recommend/store/recommend.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {}
+
+const recommendSlice = createSlice({
+  name: 'recommend',
+  initialState,
+  reducers: {},
+})
+
+export default recommendSlice.reducer
