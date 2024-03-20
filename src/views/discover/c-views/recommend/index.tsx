92b869d5f88d4919e15621c4065dfc2be9112654
youthOfMe <2040690180@qq.com>
2024-03-20 23:48:06 +0800

    feat: 搭建首页-推荐页的基本架构

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,6 +1,8 @@
 import xhRequest from '@/service'
 import { AxiosHeaders } from 'axios'
 import React, { ReactNode, memo, useEffect, useState } from 'react'
+import { RecommendWrapper } from './style'
+import TopBanner from './c-cpns/top-banner'
 
 interface IProps {
   children?: ReactNode
@@ -21,7 +23,15 @@ interface IBannerData {
 const Recommend: React.FC<IProps> = () => {
   const [banners, setBanners] = useState<IBannerData[]>([])
 
-  return <div>推荐吖</div>
+  return (
+    <RecommendWrapper>
+      <TopBanner></TopBanner>
+      <div className="content wrap-v2">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </RecommendWrapper>
+  )
 }
 
 export default memo(Recommend)
