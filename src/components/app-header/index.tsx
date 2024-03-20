5f5888eb824cf0d431a9f667f219247d5c4ccab2
youthOfMe <2040690180@qq.com>
2024-03-20 23:13:51 +0800

    feat: 完善appHeader左部的逻辑

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -3,12 +3,41 @@ import type { ReactNode } from 'react'
 import { HeaderLeft, HeaderRight, HeaderWrapper } from './style'
 
 import HeaderTitles from '@/assets/data/header-titles.json'
+import { NavLink } from 'react-router-dom'
 
 interface IProps {
   children?: ReactNode
 }
 
+interface TitleType {
+  type: string
+  title: string
+  link: string
+}
+
 const AppHerder: React.FC<IProps> = (props) => {
+  /** 组件的展示逻辑 */
+  function showItem(item: TitleType) {
+    if (item.type === 'path') {
+      return (
+        <NavLink
+          to={item.link}
+          className={({ isActive }) => {
+            return isActive ? 'active' : undefined
+          }}
+        >
+          {item.title}
+        </NavLink>
+      )
+    } else {
+      return (
+        <a href={item.link} target="_blink" rel="noreferrer">
+          {item.title}
+        </a>
+      )
+    }
+  }
+
   return (
     <HeaderWrapper>
       <div className="content wrap-v1">
@@ -19,8 +48,8 @@ const AppHerder: React.FC<IProps> = (props) => {
           <div className="title-list">
             {HeaderTitles.map((item) => {
               return (
-                <div className="item active" key={item.link}>
-                  <a href="">{item.title}</a>
+                <div className="item" key={item.link}>
+                  {showItem(item)}
                 </div>
               )
             })}
