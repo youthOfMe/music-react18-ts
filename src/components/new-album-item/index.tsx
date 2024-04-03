f2fa213001f5219b4eeb6c2fcb0fdb210dc05bce
youthOfMe <2040690180@qq.com>
2024-04-03 21:47:41 +0800

    feat: 封装好new-album-item

--- src/components/new-album-item/index.tsx
+++ src/components/new-album-item/index.tsx
@@ -1,12 +1,27 @@
 import React, { ReactNode, memo } from 'react'
 import { AlbumItemWrapper } from './style'
+import { getImageSize } from '@/utils/format'
 
 interface IProps {
   children?: ReactNode
+  itemData: any
 }
 
-const NewAlbumItem: React.FC<IProps> = () => {
-  return <AlbumItemWrapper></AlbumItemWrapper>
+const NewAlbumItem: React.FC<IProps> = (props) => {
+  const { itemData } = props
+
+  return (
+    <AlbumItemWrapper>
+      <div className="top">
+        <img src={getImageSize(itemData.picUrl, 100, 100)} alt="" />
+        <a href="#" className="cover sprite_cover"></a>
+      </div>
+      <div className="bottom">
+        <div className="name">{itemData.name}</div>
+        <div className="artist">{itemData.artist.name}</div>
+      </div>
+    </AlbumItemWrapper>
+  )
 }
 
 export default memo(NewAlbumItem)
