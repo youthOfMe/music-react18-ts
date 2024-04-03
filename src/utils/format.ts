a6b00f3721e37e155cdaf05431e0d2b5eed4a77e
youthOfMe <2040690180@qq.com>
2024-04-03 19:39:15 +0800

    feat: 进行封装格式化播放量的函数

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,3 +1,11 @@
 export function getImageSize(imageUrl: string, width: number, height: number) {
   return imageUrl + `param=${width}x${height}`
 }
+
+export function formatCount(count: number) {
+  if (count > 100000) {
+    return Math.floor(count / 10000) + '万'
+  } else {
+    return count
+  }
+}
