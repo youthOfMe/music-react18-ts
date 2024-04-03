f4e395999b00bfe0f4cdcb22b856e5435349fcdb
youthOfMe <2040690180@qq.com>
2024-04-03 16:21:45 +0800

    feat: 整理代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
 import React, { Suspense, memo } from 'react'
-import { Link, useRoutes } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 import routes from './router'
-import { useAppSelector, useAppDispatch, shallowEqualApp } from './store'
-import { changeMessageAction } from './store/modules/counter'
 import AppHeader from './components/app-header'
-// import type { IRootState } from './store'
-
-// interface IStoreState {
-//   counter: {
-//     count: number
-//     message: string
-//   }
-// }
-
-// const state = store.getState()
-// type GetStatenType = typeof store.getState
-// type IRootState = ReturnType<GetStatenType>
 
 function App() {
-  const dispatch = useAppDispatch()
-
-  const { count, message } = useAppSelector(
-    (state) => ({
-      count: state.counter.count,
-      message: state.counter.message,
-    }),
-    shallowEqualApp,
-  )
-
-  /** 时间处理函数 */
-  function handleChangeMessage() {
-    // console.log('handleChangeMessage')
-    dispatch(changeMessageAction('555'))
-  }
-
   return (
     <div className="App">
       <AppHeader></AppHeader>
--- src/views/discover/index.tsx
+++ src/views/discover/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, Suspense, memo } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import NavBar from './c-cpns/nav-bar'
 
 interface IProps {
