c31e84b3a711963904e6362606e2daadcd1f4db6
youthOfMe <2040690180@qq.com>
2024-04-07 22:48:47 +0800

    feat: 完善top-ranking基本结构

--- src/views/discover/c-views/recommend/c-cpns/top-ranking/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking/index.tsx
@@ -1,12 +1,18 @@
 import React, { ReactNode, memo } from 'react'
 import { RankingWrapper } from './style'
+import AreaHeaderV1 from '@/components/area-header-v1'
 
 interface IProps {
   children?: ReactNode
 }
 
 const TopRanking: React.FC<IProps> = () => {
-  return <RankingWrapper></RankingWrapper>
+  return (
+    <RankingWrapper>
+      <AreaHeaderV1 title="榜单" moreLink="/discover/ranking"></AreaHeaderV1>
+      <div className="content"></div>
+    </RankingWrapper>
+  )
 }
 
 export default memo(TopRanking)
