43526c9c66ff55bb20796dd5da25a5c7936e594f
youthOfMe <2040690180@qq.com>
2024-04-07 23:11:53 +0800

    feat: 配置推荐页 top-ranking的store

--- src/views/discover/c-views/recommend/store/recommend.ts
+++ src/views/discover/c-views/recommend/store/recommend.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { getBanners, getHotRecommend, getNewAlbum } from '../service/recommend'
+import { getBanners, getHotRecommend, getNewAlbum, getPlaylistDetail } from '../service/recommend'
 import { BannersType, HotRecommendsType, NewAlbumsType, PersonalRecommendsType } from './type'
 
 export const fetchBannerDataAction = createAsyncThunk(
@@ -32,11 +32,46 @@ export const fetchNewAlbumsAction = createAsyncThunk('newAlbum', async (arg, { d
   dispatch(changeNewAlbumsAction(res.albums))
 })
 
+const rankingIds = [19723756, 3779629, 2884035]
+export const fetchRankingDataAction = createAsyncThunk('rankingData', (_, { dispatch }) => {
+  // 获取榜单数据
+  // 1. 每一个请求进行单独处理
+  // for (const id of rankingIds) {
+  //   getPlaylistDetail(id).then((res) => {
+  //     switch (id) {
+  //       case 19723756:
+  //         console.log('飙升榜的数据', res)
+  //         break
+  //       case 3779629:
+  //         console.log('新歌榜的数据', res)
+  //         break
+  //       case 2884035:
+  //         console.log('原创榜的数据', res)
+  //         break
+  //     }
+  //   })
+  // }
+  // 2. 将三个结果都拿到 统一放到一个数组中进行管理
+  // 保障一: 获取到所有结果后 进行dispatch操作
+  // 保障二: 获取到的结果一定有正确顺序
+  const index = 0
+  const promises: Promise<any>[] = []
+  for (const id of rankingIds) {
+    promises.push(getPlaylistDetail(id))
+  }
+
+  Promise.all(promises).then((res) => {
+    const playlists = res.map((item) => item.playlist)
+    dispatch(changeRankingsAction(playlists))
+  })
+})
+
 interface IRecomendState {
   banners: BannersType
   hotRecommends: HotRecommendsType
   personalRecommends: PersonalRecommendsType
   newAlbums: NewAlbumsType
+  rankings: any[]
 }
 
 const initialState: IRecomendState = {
@@ -44,6 +79,7 @@ const initialState: IRecomendState = {
   hotRecommends: [],
   personalRecommends: [],
   newAlbums: [],
+  rankings: [],
 }
 
 const recommendSlice = createSlice({
@@ -62,6 +98,9 @@ const recommendSlice = createSlice({
     changeNewAlbumsAction(state, { payload }) {
       state.newAlbums = payload
     },
+    changeRankingsAction(state, { payload }) {
+      state.rankings = payload
+    },
   },
 })
 
@@ -70,6 +109,7 @@ export const {
   changeHotRecommendsAction,
   changePersonalRecommendsAction,
   changeNewAlbumsAction,
+  changeRankingsAction,
 } = recommendSlice.actions
 
 export default recommendSlice.reducer
