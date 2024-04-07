02a46871f8188908120c3b73a0ce0cfc1c77717a
youthOfMe <2040690180@qq.com>
2024-04-07 22:41:14 +0800

    feat: 新增新碟上架的轮播功能事件处理函数

--- src/views/discover/c-views/recommend/c-cpns/new-album/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/new-album/index.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode, memo } from 'react'
+import React, { ElementRef, ReactNode, memo, useRef } from 'react'
 import { AlbumWrapper } from './style'
 import AreaHeaderV1 from '@/components/area-header-v1'
 import { Carousel } from 'antd'
-import { useAppSelector } from '@/store'
+import { shallowEqualApp, useAppSelector } from '@/store'
 import NewAlbumItem from '@/components/new-album-item'
 
 interface IProps {
@@ -10,18 +10,32 @@ interface IProps {
 }
 
 const NewAblums: React.FC<IProps> = () => {
+  /** 进行定义内部数据 */
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+
   /** 从redux中获取数据 */
-  const { newAlbums } = useAppSelector((state) => ({
-    newAlbums: state.recommmend.newAlbums,
-  }))
+  const { newAlbums } = useAppSelector(
+    (state) => ({
+      newAlbums: state.recommmend.newAlbums,
+    }),
+    shallowEqualApp,
+  )
+
+  /** 时间处理函数 */
+  function handlePrevClick() {
+    bannerRef.current?.prev()
+  }
+  function handleNextClick() {
+    bannerRef.current?.next()
+  }
 
   return (
     <AlbumWrapper>
       <AreaHeaderV1 title="新碟商家" moreLink="/discover/album"></AreaHeaderV1>
       <div className="content">
-        <button className="arrow arrow-left sprite_02"></button>
+        <button className="arrow arrow-left sprite_02" onClick={handlePrevClick}></button>
         <div className="banner">
-          <Carousel dots={false} speed={2000}>
+          <Carousel ref={bannerRef} dots={false}>
             {[0, 1].map((item) => {
               return (
                 <div key={item}>
@@ -36,7 +50,7 @@ const NewAblums: React.FC<IProps> = () => {
             })}
           </Carousel>
         </div>
-        <button className="arrow arrow-right sprite_02"></button>
+        <button className="arrow arrow-right sprite_02" onClick={handleNextClick}></button>
       </div>
     </AlbumWrapper>
   )
