1bf06da2538e11311f9a43237b06632e71bfa5b4
youthOfMe <2040690180@qq.com>
2024-04-10 23:11:27 +0800

    feat: 完善top-ranking-item的结构

--- src/views/discover/c-views/recommend/c-cpns/top-ranking-item/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking-item/index.tsx
@@ -1,11 +1,52 @@
 import React, { ReactNode, memo } from 'react'
+import { RankingItemWrapper } from './style'
+import { getImageSize } from '@/utils/format'
 
 interface IProps {
   children?: ReactNode
 }
 
-const TopRankingItem: React.FC<IProps> = () => {
-  return <div>TopRankingItem</div>
+const TopRankingItem: React.FC<IProps> = (props) => {
+  const { itemData } = props
+  const { tracks = [] } = itemData
+
+  return (
+    <RankingItemWrapper>
+      <div className="header">
+        <div className="image">
+          <img src={getImageSize(itemData.coverImge, 80, 80)} alt="" />
+          <a href="" className="sprite_cover"></a>
+        </div>
+        <div className="info">
+          <div className="name">{itemData.name}</div>
+          <div>
+            <button className="sprite_02 btn play"></button>
+            <button className="sprite_02 btn favor"></button>
+          </div>
+        </div>
+      </div>
+      <div className="list">
+        {tracks.slice(0, 10).map((item: any, index: number) => {
+          return (
+            <div className="item" key={item.id}>
+              <div className="index">{index + 1}</div>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="operator">
+                  <button className="btn sprite_02 play"></button>
+                  <button className="btn sprite_icon2 play"></button>
+                  <button className="btn sprite_02 favor"></button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className="footer">
+        <a href="/dicover/niubi">查看全部 &gt;</a>
+      </div>
+    </RankingItemWrapper>
+  )
 }
 
 export default memo(TopRankingItem)
